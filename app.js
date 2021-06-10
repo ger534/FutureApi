f7fd0362489b07de647fe570b59e7789d8081933
ger534 <gergerger2009@gmail.com>
2021-06-09 23:25:18 -0600

    test

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 //import cors module
 const cors = require('cors');
@@ -12,20 +11,16 @@ var futureRouter = require('./routes/future');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 //enable all cors
 app.use(cors());
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+/*
+
 app.use('/future', futureRouter);
 
 // catch 404 and forward to error handler
@@ -42,6 +37,6 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
-});
+}); */
 
 module.exports = app;
