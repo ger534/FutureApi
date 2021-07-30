d6a1795fb9b5aa3494af5cbcb2bd39a9509185cf
ger534 <gergerger2009@gmail.com>
2021-07-30 00:19:55 -0600

    download the whole novel and cap 1

--- routes/future.js
+++ routes/future.js
@@ -13,7 +13,7 @@ router.post('/chapter', (req, res) => {
 
     const chapter = req.body.chapter
     //Output the book to console for debugging
-    console.log("Sending chapter "+ chapter + "...");
+    console.log("Sending chapter " + chapter + "...");
 
     //read txt
     const textPath = path.join(__dirname, '..', 'texts', chapter + '.html');
@@ -23,40 +23,78 @@ router.post('/chapter', (req, res) => {
 
 })
 
-router.post('/download', (req, res) => {
+router.post('/download', async function (req, res) {
 
     const chapter = req.body.chapter
-    //Output the book to console for debugging
-    console.log("Downloading chapter "+ chapter + "...");
 
-    //creates empty pdf to write later
-    let pdfPath = path.join(__dirname, '..', 'pdfs', chapter + '.pdf');
-    fs.closeSync(fs.openSync(pdfPath, 'w'))
+    if (chapter === "tecnotopia") {
 
-    //read txt
-    const textPath = path.join(__dirname, '..', 'texts', chapter + '.html');
-    const data = fs.readFileSync(textPath, 'utf8')
+        //Output the book to console for debugging
+        console.log("Downloading tecnotopia...");
 
-    //write pdf 
-    pdf.create(data).toFile(pdfPath, function (err, response) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Creating pdf...");
+        //creates empty pdf to write later
+        let pdfPath = path.join(__dirname, '..', 'pdfs', 'tecno_topia.pdf');
+        fs.closeSync(fs.openSync(pdfPath, 'w'))
 
-            const src = fs.createReadStream(pdfPath);
+        //read txt
+        //TODO: improve this
+        const textPath1 = path.join(__dirname, '..', 'texts', '0-terms_and_conditions.html');
+        const data1 = fs.readFileSync(textPath1, 'utf8')
 
-            res.writeHead(200, {
-                'Content-Type': 'application/pdf',
-                'Content-Disposition': 'attachment; filename=sample.pdf',
-                'Content-Transfer-Encoding': 'Binary'
-            });
+        const textPath2 = path.join(__dirname, '..', 'texts', '1-hello_world.html');
+        const data2 = fs.readFileSync(textPath2, 'utf8')
+        
+        //write pdf 
+        pdf.create(data1+'\n'+data2).toFile(pdfPath, function (err, response) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Creating pdf...");
 
-            src.pipe(res);
-            console.log("file sent")
-        }
-    });
+                const src = fs.createReadStream(pdfPath);
 
+                res.writeHead(200, {
+                    'Content-Type': 'application/pdf',
+                    'Content-Disposition': 'attachment; filename=sample.pdf',
+                    'Content-Transfer-Encoding': 'Binary'
+                });
+                src.pipe(res);
+                console.log("file sent")
+            }
+        });
+    } else {
+
+        //Output the book to console for debugging
+        console.log("Downloading chapter " + chapter + "...");
+
+        //creates empty pdf to write later
+        let pdfPath = path.join(__dirname, '..', 'pdfs', chapter + '.pdf');
+        fs.closeSync(fs.openSync(pdfPath, 'w'))
+
+        //read txt
+        const textPath = path.join(__dirname, '..', 'texts', chapter + '.html');
+        const data = fs.readFileSync(textPath, 'utf8')
+
+        //write pdf 
+        pdf.create(data).toFile(pdfPath, function (err, response) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Creating pdf...");
+
+                const src = fs.createReadStream(pdfPath);
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/pdf',
+                    'Content-Disposition': 'attachment; filename=sample.pdf',
+                    'Content-Transfer-Encoding': 'Binary'
+                });
+
+                src.pipe(res);
+                console.log("file sent")
+            }
+        });
+    }
 
 });
 
