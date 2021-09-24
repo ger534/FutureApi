264ca0f352cc7ac4444480e46db8cf96f689b556
ger534 <gergerger2009@gmail.com>
2021-09-23 23:11:50 -0600

    fine tuning

--- games/games.js
+++ games/games.js
@@ -4,7 +4,7 @@ const games = [
         data: [
             {
                 id: "start",
-                html: "<p>Empresa A concluye el contrato con tu Empleador B.</p>\
+                html: "<p id=\"\">Empresa A concluye el contrato con tu Empleador B.</p>\
                         <p>&gt; Empleador B te despide.\
                             <a href=\"#no_contrato_b_contrato_a_1\">NEXT</a></p>\
                         <p>&gt; Empleador B te asigna otro contrato.\
@@ -12,7 +12,7 @@ const games = [
             },
             {
                 id: "no_contrato_b_contrato_a_1",
-                html: "<p>¿Empresa A te contrata?.</p>\
+                html: "<p id=\"no_contrato_b_contrato_a_1\">¿Empresa A te contrata?.</p>\
                         <p>&gt; Sí.\
                         <a href=\"#contrato_a\">NEXT</a></p>\
                         <p>&gt; No.\
@@ -20,28 +20,28 @@ const games = [
             },
             {
                 id: "contrato_a",
-                html: "<p>Empleado mejor empleado. Felicidades.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
+                html: "<p id=\"contrato_a\">Empleado mejor empleado. Felicidades.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
             },
             {
                 id: "contrato_b",
-                html: "<p>Empleado. Nada cambió.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
+                html: "<p id=\"contrato_b\">Empleado. Nada cambió.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
             },
             {
                 id: "no_contrato",
-                html: "<p>Empleado desempleado. Mala suerte.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
+                html: "<p id=\"no_contrato\">Empleado desempleado. Mala suerte.<br/><a href=\"/game\">>REINICIAR</a></p><br/>"
             },
             {
                 id: "contrato_b_1",
-                html: "<p>¿Empresa A te contrata?.</p>\
+                html: "<p id=\"contrato_b_1\">¿Empresa A te contrata?.</p>\
                         <p>&gt; Sí.<a href=\"#contrato_a_or_contrato_b\">NEXT</a></p> \
                         <p>&gt; No.<a href=\"#contrato_b\">NEXT</a></p><br/>"
             },{
                     id: "contrato_a_or_contrato_b",
-                    html: "<div><p>¿Empresa A o Empleador B?.</p>\
+                    html: "<p id=\"contrato_a_or_contrato_b\">¿Empresa A o Empleador B?.</p>\
                             <p>&gt; Empresa A.\
                             <a href=\"#contrato_a\">NEXT</a></p>\
                             <p>&gt; Empleador B.\
-                            <a href=\"#contrato_b\">NEXT</a></p><br/></div>"
+                            <a href=\"#contrato_b\">NEXT</a></p><br/>"
                 }
         ]
     },
