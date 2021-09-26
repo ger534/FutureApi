f46dd8d1ad93e6b23ab8d912fe3936b420fc8daf
ger534 <gergerger2009@gmail.com>
2021-09-26 15:37:16 -0600

    better management of data

--- games/games.js
+++ games/games.js
@@ -4,72 +4,60 @@ const games = [
         data: [
             {
                 id: "start",
-                html: "<p id=\"\">Empresa A concluye el contrato con tu Empleador B.</p>",
-                no_contrato_b_contrato_a_1: "<p>&gt; Empleador B te despide.</p>",
-                contrato_b_1: "<p>&gt; Empleador B te asigna otro contrato.</p>",
-                html_options: "<p>&gt; Empleador B te despide.\
-                                <a href=\"#no_contrato_b_contrato_a_1\">NEXT</a></p>\
-                                <p>&gt; Empleador B te asigna otro contrato.\
-                                <a href=\"#contrato_b_1\">NEXT</a></p>\</p><br/>"
+                text: "Empresa A concluye el contrato con tu Empleador B.",
+                options: [
+                    { id: "no_contrato_b_contrato_a_1", option: "> Empleador B te despide." },
+                    { id: "contrato_b_1", option: "> Empleador B te asigna otro contrato.", }]
             },
             {
                 id: "no_contrato_b_contrato_a_1",
-                html: "<p id=\"no_contrato_b_contrato_a_1\">¿Empresa A te contrata?.</p>",
-                contrato_a: "<p>&gt; Sí.</p>",
-                no_contrato: "<p>&gt; No.</p>",
-                html_options: "<p>&gt; Sí.\
-                                <a href=\"#contrato_a\">NEXT</a></p>\
-                                <p>&gt; No.\
-                                <a href=\"#no_contrato\">NEXT</a></p><br/>"
+                text: "¿Empresa A te contrata?.",
+                options: [
+                    { id: "contrato_a", option: "> Sí." },
+                    { id: "no_contrato", option: "> No." }]
             },
             {
                 id: "contrato_a",
-                html: "<p id=\"contrato_a\">Empleado mejor empleado. Felicidades.<br/><a href=\"/game\">>REINICIAR</a></p><br/>",
-                html_options: ""
+                text: "Empleado mejor empleado. Felicidades.",
+                options: [
+                    { id: "game", option: "REINICIAR" }
+                ]
             },
             {
                 id: "contrato_b",
-                html: "<p id=\"contrato_b\">Empleado. Nada cambió.<br/><a href=\"/game\">>REINICIAR</a></p><br/>",
-                html_options: ""
+                text: "Empleado. Nada cambió.",
+                options: [
+                    { id: "game", option: "REINICIAR" }
+                ]
             },
             {
                 id: "no_contrato",
-                html: "<p id=\"no_contrato\">Empleado desempleado. Mala suerte.<br/><a href=\"/game\">>REINICIAR</a></p><br/>",
-                html_options: ""
+                text: "Empleado desempleado. Mala suerte.",
+                options: [
+                    { id: "game", option: "REINICIAR" }
+                ]
             },
             {
                 id: "contrato_b_1",
-                html: "<p id=\"contrato_b_1\">¿Empresa A te contrata?.</p>",
-                contrato_a_or_contrato_b: "<p>&gt; Sí.</p>",
-                contrato_b: "<p>&gt; No.</p>",
-                html_options: "<p>&gt; Sí.<a href=\"#contrato_a_or_contrato_b\">NEXT</a></p> \
-                                <p>&gt; No.<a href=\"#contrato_b\">NEXT</a></p><br/>"
+                text: "¿Empresa A te contrata?.",
+                options: [
+                    { id: "contrato_a_or_contrato_b", option: "> Sí." },
+                    { id: "contrato_b", option: "> No." }]
             }, {
                 id: "contrato_a_or_contrato_b",
-                html: "<p id=\"contrato_a_or_contrato_b\">¿Empresa A o Empleador B?.</p>",
-                contrato_a: "<p>&gt; Empresa A.</p>",
-                contrato_b: "<p>&gt; Empleador B.</p>",
-                html_options: "<p>&gt; Empresa A.\
-                                <a href=\"#contrato_a\">NEXT</a></p>\
-                                <p>&gt; Empleador B.\
-                                <a href=\"#contrato_b\">NEXT</a></p><br/>"
+                text: "¿Empresa A o Empleador B?.",
+                options: [
+                    { id: "contrato_a", option: "> Empresa A." },
+                    { id: "contrato_b", option: "> Empleador B.", }]
             }
         ]
     }
 ]
 
 const screenMapper = (game, screens) => {
-    let screensFiltered = [...game.data.filter(screen => screens.includes(screen.id))].sort(function (a, b) {
+    let currentGame = [...game.data.filter(screen => screens.includes(screen.id))].sort(function (a, b) {
         return screens.indexOf(a.id) - screens.indexOf(b.id);
     });
-    let currentGame = []
-    for(let i = 0; i < screensFiltered.length; i++){
-        if(i !== screensFiltered.length-1){
-            currentGame.push({id: screensFiltered[i].id, html: screensFiltered[i].html + screensFiltered[i][screens[i+1]]})
-        }else{
-            currentGame.push({id: screensFiltered[i].id, html: screensFiltered[i].html + screensFiltered[i].html_options})
-        }
-    }
     return { id: game.id, data: currentGame }
 }
 
